cb4f5f2a33a2ef8cdb70a595ca5d60052b81c6fe
Alan Veloso <alantsv@gmail.com>
2020-07-12 13:02:49 -0300

    Add divide e conquer

--- hierarchy/SubordinateSmartContract.sol
+++ hierarchy/SubordinateSmartContract.sol
@@ -9,9 +9,10 @@ contract SubordinateSmartContract {
     address[] subordinates;
     address chief;
 
-    function sendCommand(Messages.Message memory task) public{
-        for (uint i = 0; i<subordinates.length; i++) {
-            bytes memory data = abi.encodeWithSignature("reciveCommand((address, address, string))", task);
+    function sendCommand(Messages.Message memory task) public {
+        for (uint i = 0; i < subordinates.length; i++) {
+            Messages.Message memory subtask = splitTask(task, subordinates[i]);
+            bytes memory data = abi.encodeWithSignature("reciveCommand((address, address, string))", subtask);
             address(subordinates[i]).call(data);
         }
     }
@@ -21,8 +22,12 @@ contract SubordinateSmartContract {
             msg.sender == chief
             "Only chief can send a task message"
         );
-
-        performTask(task);
+        if (checkFinalizableTask(task)) {
+            performTask(task);
+        }
+        else {
+            sendCommand(task);
+        }
     }
 
     function reciveInfo (Messages.Message memory task) public {
@@ -30,5 +35,11 @@ contract SubordinateSmartContract {
     }
 
     function performTask (Messages.Message memory task) private;
+
+    function checkFinalizableTask(Messages.Message memory task) returns (bool);
+
+    function splitTask(Messages.Message memory task, address subordinate) private returns (Messages.Message) {
+        ...
+    }
 
 }
