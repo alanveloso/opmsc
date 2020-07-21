3bb147e77c34f6725215bebc22ed50629b635f49
Alan Veloso <alantsv@gmail.com>
2020-07-21 13:09:07 -0300

    Add to libraries Item struct

---
+++ structures/libraries/Item/Item.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract Item {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Sender not authorized."
+        );
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+}
