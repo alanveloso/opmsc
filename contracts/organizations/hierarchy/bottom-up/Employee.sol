a4ac8dc3edd97f0882ea3a7f5028b4eb022e42f9
Alan Veloso <alantsv@gmail.com>
2020-08-24 19:18:19 -0300

    refectoring hierarchy organization

--- contracts/organizations/hierarchy/bottom-up/Employee.sol
+++ contracts/organizations/hierarchy/bottom-up/Employee.sol
@@ -3,22 +3,31 @@ pragma solidity >=0.4.16 <=0.7.0;
 
 abstract contract Employee {
     
+    /* Attributes */
+    address owner;
+
     address manager;
 
     mapping(address => bytes) pendingReturns;
+
+    /* Modifiers */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     modifier onlyManager() {
         require(manager == msg.sender);
         _;
     }
 
-    // Perform a task according to specifications.
-    function run(bytes memory info) public virtual;
+    /* Agent */
+    constructor() {
+        owner = msg.sender;
+    }
 
-    fallback() external virtual {
-        require(manager != address(0));
-        bytes memory data = bytes(abi.encodePacked(msg.data, msg.sender));
-        send(manager, data);
+    function setup(address _manager) public onlyOwner {
+        manager = _manager;
     }
 
     function send(address to, bytes memory data) public {
@@ -26,9 +35,37 @@ abstract contract Employee {
         require(success);
     }
 
-    function receive() onlyManager public {
-        (bytes memory result, address origin) = abi.decode(msg.data, (bytes, address));
-        pendingReturns[origin] = result;
-    }
+    // pick up messages from its message queue
+    // function receive() onlyManager public {
+    //     // Process the message
+    //     (bytes memory result, address origin) = abi.decode(msg.data, (bytes, address));
+    //     pendingReturns[origin] = result;
+    // }
 
+    /* Behaviours */
+    // fallback() external virtual {
+    //     require(manager != address(0));
+    //     bytes memory data = bytes(abi.encodePacked(msg.data, msg.sender));
+    //     send(manager, data);
+    // }
+
+    // e.g. for mock
+    // function purchase(string title, uint maxPrice, Date deadline) public pure returns (uint) {
+    //     uint initTime = System.currentTimeMillis();
+    //     deltaT = deadline - initTime;
+
+    //     long currentTime = System.currentTimeMillis(); 
+    //     if (currentTime > deadline) { 
+    //         // Deadline expired 
+    //         myGui.notifyUser("Cannot buy book "+title); 
+    //         // stop();
+    //     }
+    //     else { 
+    //         // Compute the currently acceptable price and start a negotiation
+    //         long elapsedTime = currentTime - initTime; 
+    //         int acceptablePrice = maxPrice * (elapsedTime / deltaT); 
+    //         // myAgent.addBehaviour(new BookNegotiator(title, acceptablePrice, this));
+
+    //     }
+    // }
 }
