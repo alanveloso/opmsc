72799c3d2672b707d76cd361417331d2f5af68cb
Alan Veloso <alantsv@gmail.com>
2020-10-16 09:26:14 -0300

    Update hierarchy structure

--- contracts/organizations/hierarchy/Manager.sol
+++ contracts/organizations/hierarchy/Manager.sol
@@ -8,7 +8,6 @@ abstract contract Manager is Employee {
     
     /* Attributes */
     address[] subordinates;
-
     mapping(address => bool) allowedSubordinates;
 
     /* Modifiers */
@@ -22,26 +21,12 @@ abstract contract Manager is Employee {
         if (_manager != address(0)) {
             manager = _manager;
         }
-        if (_subordinates.lenght > 0) {
-            for (uint i = 0; i < _subordinates.lenght; ++i) {
+        if (_subordinates.length > 0) {
+            for (uint i = 0; i < _subordinates.length; ++i) {
                 subordinates.push(_subordinates[i]);
             }
         }
     }
 
     /* Behaviours */
-    fallback() external override {
-        address origin = msg.sender;
-        if (manager == address(0)) {
-            send(msg.sender, "");
-        }
-        bytes memory data = bytes(abi.encodePacked(msg.data, origin));
-        send(manager, data);
-    }
-
-    // Subordinates call when the manager need to make a Decision.
-    function makeDecision(bytes memory info) public virtual onlySubordinates returns (bytes memory decision) {
-        // run(info)
-    }
-
 }
