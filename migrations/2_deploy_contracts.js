a8698af88fd25bb0f1d7a3e877562dd91cd94a5c
Alan Veloso <alantsv@gmail.com>
2020-10-23 10:22:00 -0300

    Remove unecessery files

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Intermediator = artifacts.require("./IntermediatorMock.sol");
-var Federated = artifacts.require("./FederatedMock.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Intermediator);
-    deployer.deploy(Federated);
-};
