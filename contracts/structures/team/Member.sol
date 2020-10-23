afe0f080bdd17994495202dcfa1cd84a2200dd72
Alan Veloso <alantsv@gmail.com>
2020-10-23 09:25:57 -0300

    Simplificate team member contract

--- contracts/structures/team/Member.sol
+++ contracts/structures/team/Member.sol
@@ -1,58 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <=0.7.0;
 
-abstract contract Manager {
+import { Agent } from "../../bade/core/Agent.sol";
+
+contract Manager is Agent {
     
-    /* Attributes */
-    address owner;
-    address[] partners;
-    mapping(address => bool) allowedPartners;
-    bytes jointMentalState; // Any thing.
+    address[] teammate;
+    mapping(address => bool) allowedTeammate;
 
-
-    /* Modifiers */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
+    /// @notice Allow only teammate.
+    modifier onlyTeammate() {
+        require(allowedTeammate[msg.sender]);
         _;
     }
-
-    modifier onlyPartners() {
-        require(allowedPartners[msg.sender]);
-        _;
-    }
-
-    /* Agent */
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function setup(address[] memory _partners) public onlyOwner {
-        if (_partners.length > 0) {
-            for (uint i = 0; i < _partners.length; ++i) {
-                partners.push(_partners[i]);
-                allowedPartners[_partners[i]] = true;
-            }
-        }
-    }
-
-    function send(address to, bytes memory data) public {
-        (bool success, ) = to.call(data);
-        require(success);
-    }
-
-    /* Behaviours */
-
-    function shareJMS() private {
-        if (partners.length > 0) {
-            for (uint i = 0; i < partners.length; ++i) {
-                send(partners[i], jointMentalState);
-            }
-        }
-    }
-
-    function updateJMS() public {
-        bytes memory sharedMS = abi.decode(msg.data[4:], (bytes));
-        jointMentalState = sharedMS;
-    }
-
 }
